706d61f094152604b9a6ed1ec9d0e6e232c5a9c3
crazymarty <62768595+crazymarty@users.noreply.github.com>
2020-10-21 21:13:46 +0530

    Update infixio.py

--- infixio.py
+++ infixio.py
@@ -4,7 +4,7 @@ import sys
 import traceback
 import aiohttp
 import discord
-from discord.ext import flags, commands, tasks
+from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions
 import os
 
